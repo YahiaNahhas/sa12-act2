60eab98eb0f12205db611581dee2a9ebbaea473e
Yahia Nahhas <yahianahhas15@gmail.com>
2024-04-22 12:57:30 -0500

    Redo of Error

--- prob1.rb
+++ prob1.rb
@@ -1,15 +1,16 @@
 require 'httparty'
 require 'json'
 
-  username = "YahiaNahhas"
-  url = "https://api.github.com/users/#{username}/repos"
-  response = HTTParty.get(url)
-  source = JSON.parse(response.body)
+username = "YahiaNahhas"
+url = "https://api.github.com/users/#{username}/repos"
 
-  most_starred = source.max_by { |repo| repo['stargazers_count'] }
+response = HTTParty.get(url, headers: {"User-Agent" => "Request"})
+repos = JSON.parse(response.body)
 
-  puts "Most Starred Repository:"
-  puts "Name: #{most_starred['name']}"
-  puts "Description: #{most_starred['description']}"
-  puts "Stars: #{most_starred['stargazers_count']}"
-  puts "URL: #{most_starred['html_url']}"
+most_starred = repos.max_by { |repo| repo["stargazers_count"] }
+
+    puts "Most Starred Repository:"
+    puts "Name: #{most_starred['name']}"
+    puts "Description: #{most_starred['description']}"
+    puts "Stars: #{most_starred['stargazers_count']}"
+    puts "URL: #{most_starred['html_url']}"
