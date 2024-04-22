69ba87c22d9807b3a88202933aeeb7b30571afa9
Yahia Nahhas <yahianahhas15@gmail.com>
2024-04-22 13:00:35 -0500

    Redo of Error, Problem fixed

--- prob3.rb
+++ prob3.rb
@@ -5,6 +5,6 @@ location = "London"
 response = HTTParty.get("http://worldtimeapi.org/api/timezone/#{area}/#{location}")
 source = JSON.parse(response.body)
 
-current_time = source['datetime']
+current_time = Time.parse(source["datetime"]).strftime("%Y-%m-%d %H:%M:%S")
 
 puts "Current Time in #{area}/#{location}: #{current_time}"
